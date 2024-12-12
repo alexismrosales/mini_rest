f62dced91d876188e1f3eef269cc19dd21d5393f
AlexisMRosales <alexismarquezrosales@gmail.com>
2024-12-12 17:31:30 -0600

    server functions modified

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
 //! # mini_rest
-//! Este crate provee un servidor HTTP básico con funciones para manejar direcciones, iniciar servidores, y más.
+//!This crate provides a basic HTTP server with functions for handling addresses, starting servers, and more.
 //!
-//! ## Ejemplo
-//! ```rust
+//! ## Example of use
+//! ```rust,no_run
 //! use mini_rest::server::{self, ServerActions};
 //!
 //! let addr = String::from("127.0.0.1:8080");
 //! let mut server = server::new(addr);
 //!
-//! server.start();
 //! ```
 
 pub mod server;
--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,15 @@
-use std::net::{TcpListener, TcpStream};
+use std::{
+    future::Future,
+    net::{TcpListener, TcpStream},
+    pin::Pin,
+};
 
 pub struct Server {
     address: String,
 }
 
 pub trait ServerActions {
-    fn start(&mut self);
+    fn start(self) -> Pin<Box<dyn Future<Output = ()> + Send>>;
 }
 
 pub trait ServerInfo {
@@ -23,8 +27,7 @@ pub trait ServerInfo {
     /// # Example
     /// ```
     /// use mini_rest::server::{self, ServerInfo};
-    /// let server = server::new("127.0.0.1:8080".to_string());
-    /// assert_eq!(server.ip(), "127.0.0.1");
+    /// let server = server::new("127.0.0.1:8080".to_string()); assert_eq!(server.ip(), "127.0.0.1");
     /// ```
     fn ip(&self) -> &str;
     /// Returns the port where server is listening.
@@ -38,10 +41,21 @@ pub trait ServerInfo {
 }
 
 impl ServerActions for Server {
-    fn start(&mut self) {
-        if let Err(e) = start(self) {
-            eprintln!("Failed to start server: {}", e);
-        }
+    /// To use the `start` function, you need to initialize an async runtime, such as Tokio. Here's an example:
+    /// ```
+    /// use mini_rest::server::{self, ServerActions};
+    /// #[tokio::main]
+    /// async fn main() {
+    ///     let mut server = server::new("127.0.0.1:8080".to_string());
+    ///     server.start().await;
+    /// }
+    /// ```
+    fn start(self) -> Pin<Box<dyn Future<Output = ()> + Send>> {
+        Box::pin(async move {
+            if let Err(e) = start_server(self).await {
+                eprintln!("Error: {}", e);
+            }
+        })
     }
 }
 
@@ -71,20 +85,20 @@ impl ServerInfo for Server {
 /// # Paramrests
 /// - `address`: An optional address for the server.
 /// # Returns
-/// A `Server` struct that allows you to execute main actions like starting the server or adding routes.
+/// A `Server` instance that supports main actions like starting the server or retrieving its details.
 pub fn new(addr: String) -> Server {
     Server { address: addr }
 }
 
-fn start(server: &mut Server) -> Result<(), Box<dyn std::error::Error>> {
+/// Starts the HTTP server asynchronously.
+///
+/// This function performs the actual logic for starting the server.
+/// It is separate from the trait `ServerActions` to avoid conflicts and allow more flexibility.
+async fn start_server(server: Server) -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind(server.address.clone())?;
-    println!("Starting server at {}...", server.address);
-    for stream in listener.incoming() {
-        handle_client(stream?);
-    }
+    println!("Starting listening at {}...", server.address);
+
     Ok(())
 }
 
-fn handle_client(stream: TcpStream) {
-    // ...
-}
+fn handle_client(mut stream: TcpStream) {}
